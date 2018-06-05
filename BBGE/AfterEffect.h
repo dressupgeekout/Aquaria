3793afe16bbd84c54b548f12bed9626026fd1ce4
Charlotte Koch <dressupgeekout@gmail.com>
2018-06-04 23:43:42 -0700

    fix include guards for aftereffect

--- BBGE/AfterEffect.h
+++ BBGE/AfterEffect.h
@@ -18,8 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
-#ifndef __after_effect__
-#define __after_effect__
+
+#ifndef BBGE_AFTEREFFECT_H
+#define BBGE_AFTEREFFECT_H
 
 #include "Core.h"
 
@@ -128,7 +129,4 @@ protected:
 
 };
 
-
-#endif
-
-
+#endif /* BBGE_AFTEREFFECT_H */
